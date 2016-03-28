0a63635ba89888e13b4d98e9a5fbc05179fa2976
manikandan0603 <manikandan0603@gmail.com>
2016-03-28 17:20:21 +0530

    users controller added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :name => 'First User', :email => 'user1@gmail.com', :password => 'admin123', :password_confirmation => 'admin123'
+puts 'New user created: ' << user.name
+user2 = User.create! :name => 'Second User', :email => 'user2@gmail.com', :password => 'admin123', :password_confirmation => 'admin123'
+puts 'New user created: ' << user2.name
